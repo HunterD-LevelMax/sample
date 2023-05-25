72d764424f469f841912cf50495415d037b1bd88
hunterd <warsvitaly@gmail.com>
2023-05-26 00:52:37 +0800

    init

--- app/src/main/java/com/template/sample/utils.kt
+++ app/src/main/java/com/template/sample/utils.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
-import android.os.Build
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
